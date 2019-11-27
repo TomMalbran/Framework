0e605b7ebd47e2e84b05002514284462f6b1f3a5
Tomas Malbran <malbrantomas@gmail.com>
2019-11-27 00:43:13 -0300

    Auth Spam

---
+++ src/Auth/Spam.php
@@ -0,0 +1,55 @@
+<?php
+namespace Framework\Auth;
+
+use Framework\Schema\Factory;
+use Framework\Schema\Query;
+use Framework\Utils\Utils;
+
+/**
+ * The Auth Spam
+ */
+class Spam {
+    
+    private static $loaded = false;
+    private static $schema = null;
+    
+    
+    /**
+     * Loads the Spam Schema
+     * @return Schema
+     */
+    public static function getSchema() {
+        if (!self::$loaded) {
+            self::$loaded = false;
+            self::$schema = Factory::getSchema("spam");
+        }
+        return self::$schema;
+    }
+    
+    
+    
+    /**
+     * Proection against multiple logins in a few seconds
+     * @return boolean
+     */
+    public static function protection() {
+        $schema = self::getSchema();
+        $ip     = Utils::getIP();
+        
+        // Delete old entries
+        $schema->remove(Query::create("time", "<", time() - 2)->add("ip", "=", $ip));
+        $schema->remove(Query::create("time", "<", time() - 3));
+        
+        // Check if there is still an entry for the given ip
+        if ($schema->exists(Query::create("ip", "=", $ip))) {
+            return true;
+        }
+        
+        // Add a new entry
+        $schema->replace([
+            "ip"   => $ip,
+            "time" => time(),
+        ]);
+        return false;
+    }
+}
