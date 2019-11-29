2fc6c505b79284ec217157ce623bfeb07cbbf6a3
Tomas Malbran <malbrantomas@gmail.com>
2019-11-29 15:20:25 -0300

    Enum based on types

--- src/Utils/Enum.php
+++ src/Utils/Enum.php
@@ -27,31 +27,26 @@ class Enum {
         if (empty(self::$cache[$class])) {
             $reflection = new ReflectionClass($class);
             $properties = $reflection->getStaticProperties();
+            $type       = "constant";
+            $data       = [];
+
+            if (!empty($properties["data"])) {
+                $data = $properties["data"];
+                $type = is_array(array_values($data)[0]) ? "map" : "array";
+            }
             self::$cache[$class] = (object)[
-                "constants" => $reflection->getConstants(),
-                "data"      => !empty($properties["data"]) ? $properties["data"] : [],
+                "constants"  => $reflection->getConstants(),
+                "data"       => $data,
+                "type"       => $type,
+                "isConstant" => $type == "constant",
+                "isArray"    => $type == "map",
+                "isMap"      => $type == "map",
             ];
         }
         return self::$cache[$class];
     }
 
-    /**
-     * Returns the Data of the Enum
-     * @return array
-     */
-    public static function getData() {
-        $cache = self::load();
-        return $cache->data;
-    }
 
-    /**
-     * Returns all the Constants
-     * @return array
-     */
-    public static function toArray() {
-        $cache = self::load();
-        return $cache->constants;
-    }
 
     /**
      * Check if is valid enum value
@@ -60,60 +55,23 @@ class Enum {
      */
     public static function isValid($value) {
         $cache = self::load();
-        if (!empty($cache->data)) {
-            return in_array($value, array_keys($cache->data));
+        if ($cache->isConstant) {
+            return in_array($value, array_values($cache->constants));
         }
-        return in_array($value, $cache->constants);
-    }
-
-
-
-    /**
-     * Returns a value from the Data
-     * @param string $name
-     * @param array  $arguments
-     * @return mixed|null
-     */
-    public static function __callStatic($name, array $arguments) {
-        $data = self::getData();
-        if (empty($data)) {
-            return null;
-        }
-
-        // Get the Key depending on the $name
-        $value = $arguments[0];
-        $key   = $name;
-        if (Strings::startsWith($name, "get")) {
-            $key    = Strings::replace($name, "get", "");
-            $key[0] = Strings::toLowerCase($key[0]);
-        }
-        
-        // Return the Value
-        if (!empty($data[$value])) {
-            if (is_array($data[$value])) {
-                return $data[$value][$key];
-            }
-            if ($key == "name") {
-                return $data[$value];
-            }
-            if ($key == "key") {
-                return $value;
-            }
-        }
-        return null;
+        return in_array($value, array_keys($cache->data));
     }
 
     /**
-     * Returns the Data from the Data property
-     * @param integer $value
-     * @return object
+     * Returns a single Value
+     * @param mixed $value
+     * @return mixed
      */
     public static function getOne($value) {
-        $data = self::getData();
-        if (!empty($data[$value])) {
-            return (object)$data[$value];
+        $cache = self::load();
+        if ($cache->isConstant) {
+            return $cache->constants[$value];
         }
-        return null;
+        return (object)$cache->data[$value];
     }
 
     /**
@@ -121,14 +79,71 @@ class Enum {
      * @return array
      */
     public static function getSelect() {
-        $data = self::getData();
-        if (empty($data)) {
-            return [];
+        $cache = self::load();
+        if ($cache->isConstant) {
+            return Utils::createSelectFromMap($cache->constants);
         }
-        $values = array_values($data);
-        if (is_array($values[0])) {
-            return Utils::createSelect($data, "key", "name");
+        if ($cache->isArray) {
+            return Utils::createSelectFromMap($cache->data);
         }
-        return Utils::createSelectFromMap($data);
+        if ($cache->isMap) {
+            return Utils::createSelect($cache->data, "key", "name");
+        }
+    }
+
+
+
+    /**
+     * Returns a value from the Data
+     * @param string $function
+     * @param array  $arguments
+     * @return mixed|null
+     */
+    public static function __callStatic($function, array $arguments) {
+        $cache = self::load();
+        $value = $arguments[0];
+
+        // For CONSTANT
+        if ($cache->isConstant) {
+            if (Strings::startsWith($function, "is")) {
+                $key = Strings::removeFromStart($function, "is");
+                return $key == $value;
+            }
+            return null;
+        }
+
+        // For ARRAY
+        if ($cache->isArray) {
+            // If the name is the "getKey" or "getIndex" return the key of the data
+            if ($function == "getKey" || $function == "getIndex") {
+                foreach ($cache->data as $key => $name) {
+                    if (Strings::isEqual($name, $value)) {
+                        return $index;
+                    }
+                }
+                return "";
+            }
+            // If the name is the "getName" or "getValue" return the value of the data
+            if ($function == "getName" || $function == "getValue") {
+                return !empty($cache->data[$value]) ? $cache->data[$value] : "";
+            }
+        }
+
+        // For MAP
+        if ($cache->isMap) {
+            // Get the Key depending on the $name
+            $key = $function;
+            if (Strings::startsWith($function, "get")) {
+                $key    = Strings::removeFromStart($function, "get", "");
+                $key[0] = Strings::toLowerCase($key[0]);
+            }
+            
+            // If the values of each element is an array try to get the "key" there
+            if (!empty($cache->data[$value])) {
+                return $cache->data[$value][$key];
+            }
+        }
+
+        return null;
     }
 }
