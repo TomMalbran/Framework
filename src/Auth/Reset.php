5b2b80fa96ccbeef9a1c76408e09b359861c517a
Tomas Malbran <malbrantomas@gmail.com>
2021-06-09 11:31:31 -0300

    Allow resets with just an email

--- src/Auth/Reset.php
+++ src/Auth/Reset.php
@@ -40,6 +40,16 @@ class Reset {
     }
 
     /**
+     * Returns the Email for the given Code
+     * @param string $code
+     * @return integer
+     */
+    public static function getEmail(string $code): string {
+        $query = Query::create("code", "=", $code);
+        return self::getSchema()->getValue($query, "email");
+    }
+
+    /**
      * Returns true if the given code exists
      * @param string $code
      * @return boolean
@@ -53,13 +63,15 @@ class Reset {
 
     /**
      * Creates and saves a recover code for the given Credential
-     * @param integer $credentialID
+     * @param integer $credentialID Optional.
+     * @param string  $email        Optional.
      * @return string
      */
-    public static function create(int $credentialID): string {
+    public static function create(int $credentialID = 0, string $email = ""): string {
         $code = Strings::randomCode(6, "ud");
         self::getSchema()->replace([
             "CREDENTIAL_ID" => $credentialID,
+            "email"         => $email,
             "code"          => $code,
             "time"          => time(),
         ]);
@@ -68,11 +80,13 @@ class Reset {
 
     /**
      * Deletes the reset data for the given Credential
-     * @param integer $credentialID
+     * @param integer $credentialID Optional.
+     * @param string  $email        Optional.
      * @return boolean
      */
-    public static function delete(int $credentialID): bool {
-        $query = Query::create("CREDENTIAL_ID", "=", $credentialID);
+    public static function delete(int $credentialID = 0, string $email = ""): bool {
+        $query = Query::createIf("CREDENTIAL_ID", "=", $credentialID);
+        $query->addIf("email", "=", $email);
         return self::getSchema()->remove($query);
     }
 
