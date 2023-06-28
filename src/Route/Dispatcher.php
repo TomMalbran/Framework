738cd56839ec4c279f09e7d992c4e87aec795166
Tomas Malbran <malbrantomas@gmail.com>
2023-06-28 20:40:34 -0300

    Simplify the Dispatcher even more

--- src/Route/Dispatcher.php
+++ src/Route/Dispatcher.php
@@ -31,58 +31,18 @@ class Dispatcher {
 
 
     /**
-     * Dispatches a Created event
-     * @param string  $keyName
-     * @param integer $elemID
-     * @param integer $parentID Optional.
-     * @return boolean
-     */
-    public static function created(string $keyName, int $elemID, int $parentID = 0): bool {
-        return self::dispatch("created", $keyName, $elemID, $parentID);
-    }
-
-    /**
-     * Dispatches a Edited event
-     * @param string  $keyName
-     * @param integer $elemID
-     * @param integer $parentID Optional.
-     * @return boolean
-     */
-    public static function edited(string $keyName, int $elemID, int $parentID = 0): bool {
-        return self::dispatch("edited", $keyName, $elemID, $parentID);
-    }
-
-    /**
-     * Dispatches a Deleted event
-     * @param string  $keyName
-     * @param integer $elemID
-     * @param integer $parentID Optional.
-     * @return boolean
-     */
-    public static function deleted(string $keyName, int $elemID, int $parentID = 0): bool {
-        return self::dispatch("deleted", $keyName, $elemID, $parentID);
-    }
-
-
-
-    /**
      * Dispatches the event
-     * @param string  $event
-     * @param string  $keyName
-     * @param integer $elemID
-     * @param integer $parentID Optional.
+     * @param string $event
+     * @param mixed  ...$params
      * @return boolean
      */
-    private static function dispatch(string $event, string $keyName, int $elemID, int $parentID = 0): bool {
+    private static function dispatch(string $event, mixed ...$params): bool {
         self::load();
         if (!self::$data[$event]) {
             return false;
         }
-        if (!self::$data[$event][$keyName]) {
-            return false;
-        }
 
-        foreach (self::$data[$event][$keyName] as $function) {
+        foreach (self::$data[$event] as $function) {
             if (Strings::contains($function, "::")) {
                 [ $module, $method ] = Strings::split($function, "::");
                 $isStatic = true;
@@ -90,8 +50,18 @@ class Dispatcher {
                 [ $module, $method ] = Strings::split($function, "->");
                 $isStatic = false;
             }
-            Router::execute($isStatic, $module, $method, $elemID, $keyName, $parentID);
+            Router::execute($isStatic, $module, $method, ...$params);
         }
         return true;
+    }
+
+    /**
+     * Returns a value depending on the call name
+     * @param string $function
+     * @param array  $arguments
+     * @return mixed
+     */
+    public static function __callStatic(string $function, array $arguments) {
+        return self::dispatch($function, ...$arguments);
     }
 }
