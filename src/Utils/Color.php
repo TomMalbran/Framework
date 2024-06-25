8d5bd13b1bfaa116ef777512274925c70d153878
Tomas Malbran <malbrantomas@gmail.com>
2024-06-25 14:33:12 -0300

    Add a new Colors enum

---
+++ src/Utils/Color.php
@@ -0,0 +1,27 @@
+<?php
+namespace Framework\Utils;
+
+use Framework\Utils\Enum;
+
+/**
+ * The Colors
+ */
+class Color extends Enum {
+
+    const None         = "#f2f2f2";
+    const Red          = "#e8384f";
+    const Orange       = "#fd612c";
+    const YellowOrange = "#fd9a00";
+    const Yellow       = "#eec300";
+    const YellowGreen  = "#a4cf30";
+    const Green        = "#62d26f";
+    const BlueGreen    = "#37c5ab";
+    const Aqua         = "#20aaea";
+    const Blue         = "#4186e0";
+    const Indigo       = "#7a6ff0";
+    const Purple       = "#aa62e3";
+    const Magenta      = "#e362e3";
+    const HotPink      = "#ea4e9d";
+    const Pink         = "#fc91ad";
+    const Gray         = "#8da3a6";
+}
