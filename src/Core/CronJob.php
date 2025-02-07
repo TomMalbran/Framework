a37630a4c448e1aa8e7d87c3f153d55a291f3ba3
Tomas Malbran <malbrantomas@gmail.com>
2025-02-07 15:43:05 -0300

    Add a new CronJob attribute class

---
+++ src/Core/CronJob.php
@@ -0,0 +1,41 @@
+<?php
+namespace Framework\Core;
+
+use Attribute;
+
+/**
+ * The CronJob Attribute
+ */
+#[Attribute]
+class CronJob {
+
+    public string $minute;
+    public string $hour;
+    public string $dayOfMonth;
+    public string $month;
+    public string $dayOfWeek;
+
+
+
+    /**
+     * The CronJob Attribute
+     * @param string $minute
+     * @param string $hour
+     * @param string $dayOfMonth
+     * @param string $month
+     * @param string $dayOfWeek
+     */
+    public function __construct(
+        string $minute,
+        string $hour,
+        string $dayOfMonth,
+        string $month,
+        string $dayOfWeek,
+    ) {
+        $this->minute     = $minute;
+        $this->hour       = $hour;
+        $this->dayOfMonth = $dayOfMonth;
+        $this->month      = $month;
+        $this->dayOfWeek  = $dayOfWeek;
+    }
+}
