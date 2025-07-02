3cfd8466ca77344d335b536e9f0ae1de05d39415
Tomas Malbran <malbrantomas@gmail.com>
2025-07-02 12:57:56 -0300

    Add an util to get an error from the Mustache template

--- src/Provider/Mustache.php
+++ src/Provider/Mustache.php
@@ -3,6 +3,7 @@ namespace Framework\Provider;
 
 use Mustache_Autoloader;
 use Mustache_Engine;
+use Exception;
 
 /**
  * The Mustache Provider
@@ -12,19 +13,41 @@ class Mustache {
     private static ?Mustache_Engine $engine = null;
 
 
+    /**
+     * Returns the Mustache Engine instance
+     * @return Mustache_Engine
+     */
+    private static function getEngine(): Mustache_Engine {
+        if (self::$engine === null) {
+            Mustache_Autoloader::register();
+            self::$engine = new Mustache_Engine();
+        }
+        return self::$engine;
+    }
+
+
 
     /**
-     * Renders a mustache template
+     * Validates a Mustache template and returns an error
+     * @param string $template
+     * @return string
+     */
+    public static function getError(string $template): string {
+        try {
+            self::getEngine()->render($template, []);
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+        return "";
+    }
+
+    /**
+     * Renders a Mustache template
      * @param string              $template
      * @param array<string,mixed> $data
      * @return string
      */
     public static function render(string $template, array $data): string {
-        if (self::$engine === null) {
-            Mustache_Autoloader::register();
-            self::$engine = new Mustache_Engine();
-        }
-
-        return self::$engine->render($template, $data);
+        return self::getEngine()->render($template, $data);
     }
 }
