d46d57d287aeacf75e14bf46a5badb9ffc943a15
Tomas Malbran <malbrantomas@gmail.com>
2025-10-21 18:46:06 -0300

    Fix the Host of the Domain Data

--- src/Provider/Type/DomainData.php
+++ src/Provider/Type/DomainData.php
@@ -14,31 +14,31 @@ class DomainData {
     // Owner DNS Record
     public bool   $ownerValid    = false;
     public string $ownerType     = "";
-    public string $ownerName     = "";
+    public string $ownerHost     = "";
     public string $ownerValue    = "";
 
     // SPF DNS Record
     public bool   $spfValid      = false;
     public string $spfType       = "";
-    public string $spfName       = "";
+    public string $spfHost       = "";
     public string $spfValue      = "";
 
     // DKIM DNS Record
     public bool   $dkimValid     = false;
     public string $dkimType      = "";
-    public string $dkimName      = "";
+    public string $dkimHost      = "";
     public string $dkimValue     = "";
 
     // Tracking DNS Record
     public bool   $trackingValid = false;
     public string $trackingType  = "";
-    public string $trackingName  = "";
+    public string $trackingHost  = "";
     public string $trackingValue = "";
 
     // DMARC DNS Record
     public bool   $dmarcValid    = false;
     public string $dmarcType     = "";
-    public string $dmarcName     = "";
+    public string $dmarcHost     = "";
     public string $dmarcValue    = "";
 
 }
