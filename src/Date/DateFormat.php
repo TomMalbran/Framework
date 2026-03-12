05a167ed2fc25aeec9826b73aebef03f2510db5a
Tomas Malbran <malbrantomas@gmail.com>
2026-03-12 01:35:26 -0300

    Add new Date Formats

--- src/Date/DateFormat.php
+++ src/Date/DateFormat.php
@@ -4,14 +4,17 @@ namespace Framework\Date;
 /**
  * The Date Formats used by the System
  */
-enum DateFormat : string {
+enum DateFormat: string {
 
     case Time           = "H:i";
 
     case Dashes         = "d-m-Y";
-    case DashesReverse  = "Y-m-d";
     case DashesTime     = "d-m-Y H:i";
     case DashesSeconds  = "d-m-Y H:i:s";
+
+    case Reverse        = "Y-m-d";
+    case ReverseTime    = "Y-m-d H:i";
+    case ReverseSeconds = "Y-m-d H:i:s";
 
     case Slashes        = "d/m/Y";
     case SlashesTime    = "d/m/Y H:i";
