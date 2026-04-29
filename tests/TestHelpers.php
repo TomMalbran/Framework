01efc4d05f4b495d629b463eb6d0e713c186ab73
Tomas Malbran <malbrantomas@gmail.com>
2026-04-28 23:04:30 -0300

    Remove the unneeded setAccessible in the Test Helpers

--- tests/TestHelpers.php
+++ tests/TestHelpers.php
@@ -22,28 +22,24 @@ trait TestHelpers {
     protected function getPrivateProperty(object $obj, string $name): mixed {
         $ref = new ReflectionClass($obj);
         $prop = $ref->getProperty($name);
-        $prop->setAccessible(true);
         return $prop->getValue($obj);
     }
 
     protected function getPrivateStaticProperty(string $class, string $name): mixed {
         $ref = new ReflectionClass($class);
         $prop = $ref->getProperty($name);
-        $prop->setAccessible(true);
         return $prop->getValue();
     }
 
     protected function setPrivateStaticProperty(string $class, string $name, mixed $value): void {
         $ref = new ReflectionClass($class);
         $prop = $ref->getProperty($name);
-        $prop->setAccessible(true);
         $prop->setValue($value);
     }
 
     protected function setPrivateProperty(object $obj, string $name, mixed $value): void {
         $ref = new ReflectionClass($obj);
         $prop = $ref->getProperty($name);
-        $prop->setAccessible(true);
         $prop->setValue($obj, $value);
     }
 
